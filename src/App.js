a06a48c1e9c75142a49c1708ae5456c90b119992
Dan Lubbers <lubbers.dan@gmail.com>
2018-06-14 12:07:03 -0600

    changed animation over to AOS

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import Logo from './logo/danlubbers-logo.png';
+// AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props) 
+      
+      // this.state = {
+      //   showLinks: false
+      // }
+      // this.handleOver = this.handleOver.bind(this);
+    }
+
+    componentDidMount() {
+      this.aos = AOS;
+      this.aos.init({duration: 3000});
+  }
+
+    // handleOver() {
+    // this.setState({showLinks: !this.state.showLinks})
+    // }
+
+  render() {
+
+    // let {showLinks} = this.state;
+    // let slideCSS = showLinks ? 'show-link show-position' : 'show-link';
+
+    return (
+      <div className="content">
+        <img className='logo' src={Logo} alt='logo' />
+        <div className='btn-container'>
+          <a href='http://www.danlubbersphotographs.com'><button className='photoBtn' data-aos='zoom-in'>Photography/Retouching</button></a>
+          <a href='http://webdev.danlubbers.com'><button className='webDevBtn' data-aos='zoom-in'>Web Development</button></a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
