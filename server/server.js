f7210d2aa42dd6bad91cabd6167a37f43166d90e
Dan Lubbers <lubbers.dan@gmail.com>
2019-08-24 14:45:49 -0400

    added hosting server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const config = require('./config')
+const app = express();
+const { PORT } = config;
+
+app.use(express.static(__dirname+'/../build'));
+
+
+app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`));
